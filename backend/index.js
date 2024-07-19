414a74e6f45d67b8d5e34a2b8cdeae42b0822800
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-07-19 15:04:56 +0530

    prod error fix

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,48 @@ const router = require("./routes");
 const app = express();
 app.use(
   cors({
-    origin: 'https://brogan-boots.vercel.app',
+    origin: 'https://brogan-boots.vercel.app, http://localhost:5173/',
     credentials: true,
   })
 );
+
+// var whitelist = ['https://brogan-boots.vercel.app', 'http://localhost:5173']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res)=> {
-  return res.send("SERVER IS RUNNING")
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", 'https://brogan-boots.vercel.app', 'http://localhost:5173');
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
-app.get("/test", (req, res)=> {
-  return res.send("This is test route")
-})
+app.get("/", (req, res) => {
+  return res.send("SERVER IS RUNNING");
+});
+
+app.get("/test", (req, res) => {
+  return res.send("This is test route");
+});
 
 app.use("/api", router);
 
